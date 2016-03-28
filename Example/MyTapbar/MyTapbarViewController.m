16c2e11bb586e908d86570737621f6a27832dd33
蔡成汉 <caichenghan@XiaoCaideMac-mini.local>
2016-03-28 11:40:54 +0800

    Example

--- Example/MyTapbar/MyTapbarViewController.m
+++ Example/MyTapbar/MyTapbarViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "MyTapbarViewController.h"
+#import "MyTapbar.h"
 
-@interface MyTapbarViewController ()
+@interface MyTapbarViewController ()<MyTapbarDelegate>
+
+@property (nonatomic , strong) MyTapbar *tapbar;
 
 @end
 
@@ -18,7 +21,31 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    _tapbar = [[MyTapbar alloc]init];
+    _tapbar.backgroundColor = [UIColor greenColor];
+    _tapbar.frame = CGRectMake(0, 100, self.view.frame.size.width, 60);
+    _tapbar.delegate = self;
+    _tapbar.showIndicator = YES;
+    _tapbar.selectFont = [UIFont systemFontOfSize:18];
+    _tapbar.unSelectFont = [UIFont systemFontOfSize:16];
+    _tapbar.dataArray = [NSArray arrayWithObjects:@"我的数据一",@"我的数据二",@"我的超长数据三",@"我的数据四",@"我的数据五",@"我的数据六",@"我的数据七",@"我的数据八",@"我的超长数据九",@"我的数据十",@"我的数据十一",@"我的数据十二", nil];
+    [self.view addSubview:_tapbar];
 }
+
+-(void)viewWillLayoutSubviews
+{
+    [super viewWillLayoutSubviews];
+    _tapbar.frame = CGRectMake(0, 100, self.view.frame.size.width, 60);
+}
+
+#pragma mark - MyTapbarDelegate
+
+-(void)myTapbar:(MyTapbar *)tapbar itemIsTouch:(NSInteger)index
+{
+    
+}
+
 
 - (void)didReceiveMemoryWarning
 {
